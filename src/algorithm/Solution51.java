f4b09e360807d7973998443d3bc682b6cb89c8a6
obt <byeongtaek12@naver.com>
2025-05-09 09:22:39 +0900

    51일

---
+++ src/algorithm/Solution51.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+class Solution51 {
+    public String solution(int[] food) {
+        String answer = "";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i<food.length; i++) {
+            for (int j = 0; j<food[i]/2; j++) {
+                sb.append(i);
+            }
+        }
+        answer = sb.toString()+"0"+sb.reverse().toString();
+        return answer;
+    }
+}
