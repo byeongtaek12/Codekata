176196f479bb631495df7b81625c5237a6a11a41
obt <byeongtaek12@naver.com>
2025-05-22 09:32:02 +0900

    59일(수정)

--- src/algorithm/Solution56.java
+++ src/algorithm/Solution56.java
@@ -1,41 +1,36 @@
 package algorithm;
 
 import java.util.*;
+
 class Solution56 {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
+
         Arrays.sort(score);
-        int min = score[0];
-        int score1 [] = new int [m];
-        int j = 0;
-        if (score.length % m != 0) {
-            for (int i = 0; i<score.length; i++) {
-                if (score[i] > min) {
-                    score1[j] = score[i];
-                    j++;
-                    Arrays.sort(score1);
-                }else if (Arrays.binarySearch(score1, 0) < 0) {
-                    score1[j] = min;
+
+        for(int i = 0; i < score.length / 2; i++) {
+            int temp = score[i];
+            score[i] = score[score.length - 1 - i];
+            score[score.length - 1 - i] = temp;
+        }
+
+        int boxCount = score.length / m;
+
+        for(int i = 0; i < boxCount; i++) {
+            int min = Integer.MAX_VALUE;
+            int start = i * m;
+            int end = start + m;
+
+
+            for(int j = start; j < end; j++) {
+                if(score[j] < min) {
+                    min = score[j];
                 }
             }
-            Arrays.sort(score1);
-            answer = score1[0] * m * score.length / m;
-        }else {
-            int s = 0;
-            int sum = 0;
-            for (int l = 0; l<score.length; l++) {
-                score1[s]= score[l];
-                if (s >= m-1) {
-                    Arrays.sort(score1);
-                    sum += score1[0] * m;
-                    s = 0;
-                }else {
-                    s++;
-                }
-            }
-            answer=sum;
+            answer += min * m;
         }
 
         return answer;
     }
+
 }
