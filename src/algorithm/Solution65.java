f15ef9fe2594a1eb907b994bbb2e1d6cf12446a6
obt <byeongtaek12@naver.com>
2025-06-24 09:49:07 +0900

    81일

--- src/algorithm/Solution65.java
+++ src/algorithm/Solution65.java
@@ -1,18 +1,38 @@
-package algorithm;
-
 class Solution65 {
 	public int solution(String s) {
 		int answer = 0;
-		int xcount = 0;
+		int xcount = 1;
 		int notxcount = 0;
 		char [] s1 = s.toCharArray();
+		char x = s1[0];
 		for (int i = 0; i<s1.length-1; i++) {
-			if (s1[i]!=s1[i+1]) {
-				answer++;
-			}else {
-
+			if (s1[i] != x){
+				continue;
 			}
-
+			for (int j= i + 1; j<s1.length; j++) {
+				if (s1[i]!=s1[j]) {
+					notxcount++;
+					if (xcount == notxcount) {
+						answer++;
+						if (s1[j+1] != s1.length+1) {
+							x = s1[j+1];
+						}
+						break;
+					}else {
+						if (s1[i+1]!=x) {
+							notxcount++;
+						}else {
+							xcount++;
+						}
+					}
+				}else {
+					if (s1[i]==x) {
+						xcount++;
+					}else {
+						notxcount++;
+					}
+				}
+			}
 		}
 
 
