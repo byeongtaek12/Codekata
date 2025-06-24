9e3970a371de0d48cca8da9fa422477a57b64201
obt <byeongtaek12@naver.com>
2025-06-24 09:48:28 +0900

    Revert "79일"
    This reverts commit a856bc5b2d861a3809a1f80abc7cffc3b94ccf43.

--- src/algorithm/Solution65.java
+++ src/algorithm/Solution65.java
@@ -1,21 +1,4 @@
 package algorithm;
 
-class Solution65 {
-	public int solution(String s) {
-		int answer = 0;
-		int xcount = 0;
-		int notxcount = 0;
-		char [] s1 = s.toCharArray();
-		for (int i = 0; i<s1.length-1; i++) {
-			if (s1[i]!=s1[i+1]) {
-				answer++;
-			}else {
-
-			}
-
-		}
-
-
-		return answer;
-	}
+public class Solution65 {
 }
