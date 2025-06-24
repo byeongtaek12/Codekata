9bcf04c646ae59441bcf01318439a376c35bad72
obt <byeongtaek12@naver.com>
2025-06-24 09:48:52 +0900

    80일

--- src/algorithm/Solution65.java
+++ src/algorithm/Solution65.java
@@ -1,4 +1,21 @@
 package algorithm;
 
-public class Solution65 {
+class Solution65 {
+	public int solution(String s) {
+		int answer = 0;
+		int xcount = 0;
+		int notxcount = 0;
+		char [] s1 = s.toCharArray();
+		for (int i = 0; i<s1.length-1; i++) {
+			if (s1[i]!=s1[i+1]) {
+				answer++;
+			}else {
+
+			}
+
+		}
+
+
+		return answer;
+	}
 }
