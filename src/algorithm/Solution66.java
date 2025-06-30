81a48e64530ab08d946ca4ec5dd4c14acb2474c1
obt <byeongtaek12@naver.com>
2025-06-30 09:46:33 +0900

    85일

---
+++ src/algorithm/Solution66.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+import java.util.*;
+class Solution66 {
+	public int[] solution(String[] keymap, String[] targets) {
+		int[] answer = new int [targets.length];
+		int key1 = 0;
+		int key2 = 0;
+
+		for (String target : targets) {
+
+		}
+
+		for (String target : targets) {
+			char charT = target.toCharArray();
+			for (int i = 0; i<charT.lenth; i++) {
+				for (String key : keymap) {
+					char charK = key.toCharArray();
+					if(charT[i] == charK[])
+				}
+			}
+		}
+
+		return answer;
+	}
+}
