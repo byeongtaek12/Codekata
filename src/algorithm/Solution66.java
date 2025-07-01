a44c507253482f71b2d397628d25da8a9fea30ab
obt <byeongtaek12@naver.com>
2025-07-01 09:37:43 +0900

    86일

--- src/algorithm/Solution66.java
+++ src/algorithm/Solution66.java
@@ -1,22 +1,29 @@
 package algorithm;
 
 import java.util.*;
+
 class Solution66 {
 	public int[] solution(String[] keymap, String[] targets) {
 		int[] answer = new int [targets.length];
 		int key1 = 0;
 		int key2 = 0;
+		List<char []> charT = new ArrayList<>();
+		List<char []> charK = new ArrayList<>();
+
 
 		for (String target : targets) {
-
+			charT.add(target.toCharArray());
+		}
+		for (String key : keymap) {
+			charK.add(key.toCharArray());
 		}
 
-		for (String target : targets) {
-			char charT = target.toCharArray();
-			for (int i = 0; i<charT.lenth; i++) {
-				for (String key : keymap) {
-					char charK = key.toCharArray();
-					if(charT[i] == charK[])
+		for (int i = 0; i<charT[0].length; i++) {
+			for (int j = 0; j<charK[0].length; j++) {
+				for (int k = 0; k<charK[1].length; k++) {
+					if (charT[0][i] == charK[0][j]) {
+
+					}
 				}
 			}
 		}
