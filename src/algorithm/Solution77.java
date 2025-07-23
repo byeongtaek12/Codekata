ec5471f1636bb41d97caca6d3672767498efff9f
obt <byeongtaek12@naver.com>
2025-07-23 12:34:29 +0900

    햄버거 만들기

---
+++ src/algorithm/Solution77.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+import java.util.Stack;
+class Solution77 {
+	public int solution(int[] ingredient) {
+		int answer = 0;
+		Stack<Integer> stack = new Stack<>();
+
+		for (int i : ingredient) {
+			stack.push(i);
+
+			if (stack.size() >= 4) {
+				int size = stack.size();
+				if (stack.get(size - 4) == 1 &&
+					stack.get(size - 3) == 2 &&
+					stack.get(size - 2) == 3 &&
+					stack.get(size - 1) == 1) {
+
+					stack.pop();
+					stack.pop();
+					stack.pop();
+					stack.pop();
+					answer++;
+				}
+			}
+		}
+		return answer;
+	}
+}
