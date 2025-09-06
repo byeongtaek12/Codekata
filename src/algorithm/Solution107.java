4cb897fc43814f15d37ba423630ffeb10422a1e7
obt <byeongtaek12@naver.com>
2025-09-06 20:28:27 +0900

    구명보트

---
+++ src/algorithm/Solution107.java
@@ -0,0 +1,21 @@
+package algorithm;
+
+import java.util.*;
+
+class Solution107 {
+	public int solution(int[] people, int limit) {
+		Arrays.sort(people);
+		int left = 0;
+		int right = people.length - 1;
+		int answer = 0;
+
+		while (left <= right) {
+			if (people[left] + people[right] <= limit) {
+				left++;
+			}
+			right--;
+			answer++;
+		}
+		return answer;
+	}
+}
